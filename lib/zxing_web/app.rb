ce37537f886c560a3391f299cdf9604210ca9b74
akm <akm2000@gmail.com>
2016-09-12 17:56:43 +0900

    Fix indentation

--- lib/zxing_web/app.rb
+++ lib/zxing_web/app.rb
@@ -8,23 +8,23 @@ require 'json'
 module ZxingWeb
   class App < Sinatra::Base
 
-get '/' do
-  "working!"
-end
+    get '/' do
+      "working!"
+    end
 
-post '/decode' do
-  files = params[:file]
-  files = [files] unless files.is_a?(Array)
-  results = files.map do |f|
-    fn = f.is_a?(String) ? f : f[:filename]
-    io = f.is_a?(String) ? f : f[:tempfile]
-    {
-      :filename => fn,
-      :result => ZXing.decode(io)
-    }
-  end
-  JSON.generate(results)
-end
+    post '/decode' do
+      files = params[:file]
+      files = [files] unless files.is_a?(Array)
+      results = files.map do |f|
+        fn = f.is_a?(String) ? f : f[:filename]
+        io = f.is_a?(String) ? f : f[:tempfile]
+        {
+          :filename => fn,
+          :result => ZXing.decode(io)
+        }
+      end
+      JSON.generate(results)
+    end
 
   end
 end
