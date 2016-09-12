5e628d1dcd2ca8c0b5140b6711fd7cc87ef3ca87
akm <akm2000@gmail.com>
2016-09-12 18:33:45 +0900

    Don't use Sinatra::Base

--- lib/zxing_web/app.rb
+++ lib/zxing_web/app.rb
@@ -1,41 +1,29 @@
 # coding: utf-8
-require 'zxing_web'
-
-require 'sinatra/base'
+require 'sinatra'
 require 'zxing'
 require 'json'
 
-module ZxingWeb
-  class App < Sinatra::Base
+def decode_files(files)
+end
 
-    get '/' do
-      "working!"
-    end
+get '/' do
+  "working!"
+end
 
-    post '/decode' do
-      results = decode_files(params[:file])
-      results.map{|r| r[:result]}.join("\n")
-    end
-
-    post '/decode.json' do
-      results = decode_files(params[:file])
-      JSON.generate(results)
-    end
-
-    private
-
-    def decode_files(files)
-      files = params[:file]
-      files = [files] unless files.is_a?(Array)
-      return files.map do |f|
-        fn = f.is_a?(String) ? f : f[:filename]
-        io = f.is_a?(String) ? f : f[:tempfile]
-        {
-          :filename => fn,
-          :result => ZXing.decode(io)
-        }
-      end
-    end
-
+post '/decode.?:format?' do
+  files = params[:file]
+  files = [files] unless files.is_a?(Array)
+  results = files.map do |f|
+    fn = f.is_a?(String) ? f : f[:filename]
+    io = f.is_a?(String) ? f : f[:tempfile]
+    {
+      :filename => fn,
+      :result => ZXing.decode(io)
+    }
+  end
+  case params[:format] || 'txt'
+  when 'json' then JSON.generate(results)
+  when 'txt'  then results.map{|r| r[:result]}.join("\n")
+  else ["Unsupported format: #{params[:foramt].inspect}", 400]
   end
 end
